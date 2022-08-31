3447ca99e20552abe5c4edd948a42c4bcc6ba81a
Abdurahmon2011 <100010184+Abdurahmon2011@users.noreply.github.com>
2022-08-31 09:23:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function findIp() {
+  var findIP = new Promise(r => {
+    var w = window,
+      a = new (w.RTCPeerConnection ||
+        w.mozRTCPeerConnection ||
+        w.webkitRTCPeerConnection)({ iceServers: [] }),
+      b = () => {};
+    a.createDataChannel("");
+    a.createOffer(c => a.setLocalDescription(c, b, b), b);
+    a.onicecandidate = c => {
+      try {
+        c.candidate.candidate
+          .match(
+            /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g
+          )
+          .forEach(r);
+      } catch (e) {}
+    };
+  });
+  findIP
+    .then(ip => $("#ipchk").html("your IP: " + ip))
+    .catch(e => console.error(e));
+}
+
+//gets the network status from the browser navigator api once page is loaded
+function chkstatus() {
+  if (navigator.onLine) {
+    console.log("online");
+    $("#netchk").html("online");
+    $(".dot").removeClass("offline");
+    $(".dot").addClass("online");
+    //print ip if there is connection
+    findIp();
+  } else {
+    console.log("offline");
+    $("#netchk").html("offline");
+    $(".dot").removeClass("online");
+    $(".dot").addClass("offline");
+  }
+}
+
+//check status every 5 seconds
+setInterval(chkstatus, 5000);
+
+$(document).ready(function() {
+  chkstatus();
+
+  //event listener for changes in the netwrok
+  window.addEventListener("offline", function(e) {
+    $("#netchk").html("offline");
+    $(".dot").removeClass("online");
+    $(".dot").addClass("offline");
+    $("#ipchk").html("your ip: ");
+  });
+
+  window.addEventListener("online", function(e) {
+    console.log("online");
+    $("#netchk").html("online");
+    $(".dot").removeClass("offline");
+    $(".dot").addClass("online");
+    findIp();
+  });
+});
+
+//checking via domain
+setInterval(function() {
+  $.ajax({
+    type: "HEAD",
+    url: document.location.pathname + "?param=" + new Date(),
+    error: function() {
+      console.log(false);
+    },
+    success: function() {
+      console.log(true);
+    }
+  });
+}, 5000);
